3ff0357914ce2311944669f48e642e0cfd81dc27
Ramiro Blas <ramiro060694@gmail.com>
2025-02-09 23:59:30 -0500

    feat: commit inicial

---
+++ src/main/java/pe/cibertec/MsLibroApplication.java
@@ -0,0 +1,13 @@
+package pe.cibertec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsLibroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsLibroApplication.class, args);
+	}
+
+}
